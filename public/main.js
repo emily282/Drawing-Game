d7292b979d97566b347d0b9954458c4c947a2a6f
Emily Thomason <emily@thomason.me.uk>
2018-09-10 11:23:09 -0400

    updated thickness functionality on toolbar

--- public/main.js
+++ public/main.js
@@ -5,9 +5,11 @@ $(function () {
   var canvas = document.getElementsByClassName('whiteboard')[0];
   var context = canvas.getContext('2d');
   var colors = document.getElementsByClassName('color');
+  var thicknesses = document.getElementsByClassName('thickness');
 
   var current = {
-    color: 'black'
+    color: 'black',
+    thickness: 10
   };
   var drawing = false;
 
@@ -20,18 +22,25 @@ $(function () {
     colors[i].addEventListener('click', onColorUpdate);
   }
 
+
+  for (var i = 0; i < thicknesses.length; i++){
+    thicknesses[i].addEventListener('click', onThicknessUpdate);
+  }
+
   socket.on('drawing', onDrawingEvent);
+
 
   // Set resize event - needed to adjust the canvas size
   window.addEventListener('resize', onResize, false);
   onResize();
 
-  function drawLine(x0, y0, x1, y1, color, emit){
+  function drawLine(x0, y0, x1, y1, color, thickness, emit){
     context.beginPath();
     context.moveTo(x0, y0);
     context.lineTo(x1, y1);
+    context.lineCap = 'round';
     context.strokeStyle = color;
-    context.lineWidth = 2;
+    context.lineWidth = thickness;
     context.stroke();
     context.closePath();
 
@@ -44,7 +53,8 @@ $(function () {
       y0: y0 / h,
       x1: x1 / w,
       y1: y1 / h,
-      color: color
+      color: color,
+      thickness: thickness
     });
   }
 
@@ -57,12 +67,12 @@ $(function () {
   function onMouseUp(e){
     if (!drawing) { return; }
     drawing = false;
-    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
+    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, current.thickness, true);
   }
 
   function onMouseMove(e){
     if (!drawing) { return; }
-    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
+    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, current.thickness, true);
     current.x = e.clientX;
     current.y = e.clientY;
   }
@@ -83,13 +93,21 @@ $(function () {
   function onDrawingEvent(data) {
     var w = canvas.width;
     var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color, data.thickness);
   }
 
   function onColorUpdate(e) {
-    console.log("ding");
-    console.log(e.target.className.split(' ')[1]);
-    current.color = e.target.className.split(' ')[1];
+    color = e.target.className.split(' ')[1];
+    for (var i = 0; i < thicknesses.length; i++){
+      thicknesses[i].style.backgroundColor = color;
+    }
+    
+    current.color = color;
+  }
+
+  function onThicknessUpdate(e) {
+    width = parseInt($(e.target).css('width'), 10);
+    current.thickness = width;
   }
 
   function onResize() {
